7399b97e07e9caa562d70ea4ec61abe00e2818c3
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2026-02-18 19:08:49 -0500

    se agregan los otros problemas resueltos pero sin GUI

---
+++ problema4/Tienda.exs
@@ -0,0 +1,29 @@
+defmodule Tienda do
+  def main do
+    valor_producto = "ingrese el valor del producto: "
+    |> Util.ingresar(:entero)
+
+    valor_descuento = "ingrese el valor del descuento (entre 0.0 y 1.0): "
+    |> Util.ingresar(:real)
+
+    calcular_descuento(valor_producto, valor_descuento)
+    |> calcular_valor_final(valor_producto)
+    |> generar_mensaje(valor_descuento)
+    |> Util.mostrar_mensaje()
+  end
+
+  defp calcular_descuento(valor_producto, valor_descuento) do
+    valor_producto * valor_descuento
+  end
+
+  defp calcular_valor_final(descuento_base, valor_producto) do
+    valor_producto - descuento_base
+  end
+
+  defp generar_mensaje(valor_final, valor_descuento) do
+    "El precio del producto con descuento del #{valor_descuento}% es #{valor_final}"
+
+  end
+end
+
+Tienda.main()
